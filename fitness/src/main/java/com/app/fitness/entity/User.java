8a5d5bcef46402ed024adfd018df06df98159b92
DhruvGargg <dhruv.2327cseai1185@kiet.edu>
2026-01-24 23:43:52 +0530

    Packages Creation

---
+++ fitness/src/main/java/com/app/fitness/entity/User.java
@@ -0,0 +1,13 @@
+package com.app.fitness.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+}
